0a81f74950432c0b9fe0c9ab430dae801f02dc3c
Vik-17 <zhakevichv@ya.ru>
2022-06-01 01:00:30 +0300

    fix commas

--- src/index.js
+++ src/index.js
@@ -1,85 +1,85 @@
-import { Article } from "./js/Article";
-import { ArticleModal } from "./js/ArticleModal";
-import { Modal } from "./js/Modal";
+import { Article } from './js/Article';
+import { ArticleModal } from './js/ArticleModal';
+import { Modal } from './js/Modal';
 
 const data = [
   {
     id: 1,
-    title: "Increasing Prosperity With Positive Thinking",
-    urlToImage: "./src/img/strategies/1.jpg",
-    tags: ["Art", "Design"],
+    title: 'Increasing Prosperity With Positive Thinking',
+    urlToImage: './src/img/strategies/1.jpg',
+    tags: ['Art', 'Design'],
     content:
-      "Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?",
-    date: "01.01.2020",
+      'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+    date: '01.01.2020',
   },
   {
     id: 2,
-    title: "Motivation Is The First Step To Success",
-    urlToImage: "./src/img/strategies/2.jpg",
-    tags: ["Culture"],
+    title: 'Motivation Is The First Step To Success',
+    urlToImage: './src/img/strategies/2.jpg',
+    tags: ['Culture'],
     content:
-      "Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?",
-    date: "01.01.2020",
+      'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+    date: '01.01.2020',
   },
   {
     id: 3,
-    title: "Success Steps For Your Personal Or Business Life",
-    urlToImage: "./src/img/strategies/3.jpg",
-    tags: ["Culture", "Design", "Art"],
+    title: 'Success Steps For Your Personal Or Business Life',
+    urlToImage: './src/img/strategies/3.jpg',
+    tags: ['Culture', 'Design', 'Art'],
     content:
-      "Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?",
-    date: "01.01.2020",
+      'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+    date: '01.01.2020',
   },
   {
     id: 4,
     title:
-      "Success Steps For Your Personal Or Business Life Plus Funny Image on the Back",
-    urlToImage: "./src/img/strategies/4.jpg",
-    tags: ["Culture", "Design", "Art"],
+      'Success Steps For Your Personal Or Business Life Plus Funny Image on the Back',
+    urlToImage: './src/img/strategies/4.jpg',
+    tags: ['Culture', 'Design', 'Art'],
     content:
-      "Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?",
-    date: "01.01.2020",
+      'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+    date: '01.01.2020',
   },
   {
     id: 5,
-    title: "Increasing Prosperity With Positive Thinking",
-    urlToImage: "./src/img/strategies/5.jpg",
-    tags: ["Design"],
+    title: 'Increasing Prosperity With Positive Thinking',
+    urlToImage: './src/img/strategies/5.jpg',
+    tags: ['Design'],
     content:
-      "Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?",
-    date: "01.01.2020",
+      'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+    date: '01.01.2020',
   },
   {
     id: 6,
-    title: "Increasing Prosperity With Positive Thinking",
-    urlToImage: "./src/img/strategies/1.jpg",
-    tags: ["Art", "Design"],
+    title: 'Increasing Prosperity With Positive Thinking',
+    urlToImage: './src/img/strategies/1.jpg',
+    tags: ['Art', 'Design'],
     content:
-      "Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?",
-    date: "01.01.2020",
+      'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+    date: '01.01.2020',
   },
   {
     id: 7,
-    title: "Motivation Is The First Step To Success",
-    urlToImage: "./src/img/strategies/2.jpg",
-    tags: ["Culture"],
+    title: 'Motivation Is The First Step To Success',
+    urlToImage: './src/img/strategies/2.jpg',
+    tags: ['Culture'],
     content:
-      "Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?",
-    date: "01.01.2020",
+      'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+    date: '01.01.2020',
   },
   {
     id: 8,
-    title: "Success Steps For Your Personal Or Business Life",
-    urlToImage: "./src/img/strategies/3.jpg",
-    tags: ["Culture", "Design", "Art"],
+    title: 'Success Steps For Your Personal Or Business Life',
+    urlToImage: './src/img/strategies/3.jpg',
+    tags: ['Culture', 'Design', 'Art'],
     content:
-      "Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?",
-    date: "01.01.2020",
+      'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+    date: '01.01.2020',
   },
 ];
 
 window.onload = function () {
-  console.log("Hello Rolling Scopes!");
+  console.log('Hello Rolling Scopes!');
 
   // Render Articles
   if (data) {
@@ -94,12 +94,12 @@ window.onload = function () {
 };
 
 const addTagsClickHandler = () => {
-  document.querySelector(".strategies__tags").addEventListener("click", (e) => {
-    if (e.target.classList.contains("tag")) {
+  document.querySelector('.strategies__tags').addEventListener('click', (e) => {
+    if (e.target.classList.contains('tag')) {
       let clickedTag = e.target;
       removeSelectedTags();
       selectClickedTag(clickedTag);
-      if (clickedTag.innerText === "All") {
+      if (clickedTag.innerText === 'All') {
         showAllStrategies();
       } else {
         filterStrategyBySelectedTag(clickedTag.innerText);
@@ -109,32 +109,32 @@ const addTagsClickHandler = () => {
 };
 
 const removeSelectedTags = () => {
-  let tags = document.querySelectorAll(".strategies__tags .tag");
+  let tags = document.querySelectorAll('.strategies__tags .tag');
   tags.forEach((tag) => {
-    tag.classList.remove("tag_selected");
-    tag.classList.add("tag_bordered");
+    tag.classList.remove('tag_selected');
+    tag.classList.add('tag_bordered');
   });
 };
 
 const selectClickedTag = (clickedTag) => {
-  clickedTag.classList.add("tag_selected");
-  clickedTag.classList.remove("tag_bordered");
+  clickedTag.classList.add('tag_selected');
+  clickedTag.classList.remove('tag_bordered');
 };
 
 const showAllStrategies = () => {
-  let strategies = document.querySelectorAll(".strategy-wrapper .strategy");
+  let strategies = document.querySelectorAll('.strategy-wrapper .strategy');
   strategies.forEach((strategy) => {
-    strategy.classList.remove("strategy_hidden");
+    strategy.classList.remove('strategy_hidden');
   });
 };
 
 const filterStrategyBySelectedTag = (selectedTag) => {
-  let strategies = document.querySelectorAll(".strategy-wrapper .strategy");
+  let strategies = document.querySelectorAll('.strategy-wrapper .strategy');
   strategies.forEach((strategy) => {
-    strategy.classList.add("strategy_hidden");
-    strategy.querySelectorAll(".tag").forEach((tag) => {
+    strategy.classList.add('strategy_hidden');
+    strategy.querySelectorAll('.tag').forEach((tag) => {
       if (tag.innerText === selectedTag) {
-        strategy.classList.remove("strategy_hidden");
+        strategy.classList.remove('strategy_hidden');
       }
     });
   });
@@ -150,8 +150,8 @@ const renderArticlesToDom = () => {
 };
 
 const getStrategiesWrapper = () => {
-  const strategiesConstainer = document.querySelector(".strategy-wrapper");
-  strategiesConstainer.innerHTML = "";
+  const strategiesConstainer = document.querySelector('.strategy-wrapper');
+  strategiesConstainer.innerHTML = '';
   return strategiesConstainer;
 };
 
@@ -165,27 +165,27 @@ const generateArticles = (data) => {
 
 const addToolsClickHandler = () => {
   document
-    .querySelector(".tools__button .button")
-    .addEventListener("click", () => {
+    .querySelector('.tools__button .button')
+    .addEventListener('click', () => {
       generateToolsModal();
     });
 };
 
 const generateToolsModal = () => {
-  renderModalWindow("Test content for Tools Modal");
+  renderModalWindow('Test content for Tools Modal');
 };
 
 const renderModalWindow = (content) => {
-  let modal = new Modal("tools-modal");
+  let modal = new Modal('tools-modal');
   modal.buildModal(content);
 };
 
 const addStrategyClickHandler = () => {
-  document.querySelector(".strategy-wrapper").addEventListener("click", (e) => {
-    if (e.target.closest(".strategy")) {
+  document.querySelector('.strategy-wrapper').addEventListener('click', (e) => {
+    if (e.target.closest('.strategy')) {
       let clickedStrategyId = e.target
-        .closest(".strategy")
-        .getAttribute("data-id");
+        .closest('.strategy')
+        .getAttribute('data-id');
       let clickedStrategyData = getClickedData(clickedStrategyId);
 
       renderArticleModalWindow(clickedStrategyData);
@@ -198,6 +198,6 @@ const getClickedData = (id) => {
 };
 
 const renderArticleModalWindow = (article) => {
-  let modal = new ArticleModal("article-modal", article);
+  let modal = new ArticleModal('article-modal', article);
   modal.renderModal();
 };
